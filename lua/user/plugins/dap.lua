dd5b6f16ba756cc52240400f09929ef4fca1d26f
lihao.hellohake <lihao.hellohake@bytedance.com>
2024-09-18 22:33:31 +0800

    update

--- lua/user/plugins/dap.lua
+++ lua/user/plugins/dap.lua
@@ -6,7 +6,7 @@ custom["leoluz/nvim-dap-go"] = {
 		"mfussenegger/nvim-dap",
 	},
 	config = function()
-		require("dap-go").setup()
+		require("dap-go").setup({})
 	end,
 }
 
@@ -29,8 +29,8 @@ custom["theHamsta/nvim-dap-virtual-text"] = {
 			enabled_commands = true,
 			highlight_changed_variables = true,
 			highlight_new_as_changed = true,
-			virt_text_pos = "eol",
-			commented = false,
+			--virt_text_pos = "eol",
+			commented = true,
 		})
 	end,
 }
@@ -119,6 +119,27 @@ custom["mfussenegger/nvim-dap"] = {
 		dap.listeners.before.event_exited.dapui_config = function()
 			dapui.close()
 		end
+		local icons = { dap = require("modules.utils.icons").get("dap") }
+		-- We need to override nvim-dap's default highlight groups, AFTER requiring nvim-dap for catppuccin.
+		vim.api.nvim_set_hl(0, "DapStopped", { fg = "#AFD700" })
+
+		vim.fn.sign_define(
+			"DapBreakpoint",
+			{ text = icons.dap.Breakpoint, texthl = "DapBreakpoint", linehl = "", numhl = "" }
+		)
+		vim.fn.sign_define(
+			"DapBreakpointCondition",
+			{ text = icons.dap.BreakpointCondition, texthl = "DapBreakpoint", linehl = "", numhl = "" }
+		)
+		vim.fn.sign_define("DapStopped", { text = icons.dap.Stopped, texthl = "DapStopped", linehl = "", numhl = "" })
+		vim.fn.sign_define(
+			"DapBreakpointRejected",
+			{ text = icons.dap.BreakpointRejected, texthl = "DapBreakpoint", linehl = "", numhl = "" }
+		)
+		vim.fn.sign_define(
+			"DapLogPoint",
+			{ text = icons.dap.LogPoint, texthl = "DapLogPoint", linehl = "", numhl = "" }
+		)
 	end,
 }
 
