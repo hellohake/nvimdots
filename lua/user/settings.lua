5a291dc2286a526218a1ae9de87eb75433521495
lihao.hellohake <lihao.hellohake@bytedance.com>
2024-09-20 13:34:31 +0800

    update

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -8,8 +8,8 @@ settings["colorscheme"] = "elflord"
 -- Filetypes in this list will skip lsp formatting if rhs is true.
 ---@type table<string, boolean>
 settings["formatter_block_list"] = {
-	lua = false, -- example
-	thrift = false,
+	--lua = false, -- example
+	thrift = true,
 }
 
 return settings
