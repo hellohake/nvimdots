ac775a049e1b46ccaef47dc53dccb4c10c3fcaf4
lihao.hellohake <lihao.hellohake@bytedance.com>
2024-10-09 21:22:14 +0800

    update

--- lua/user/keymap/editor.lua
+++ lua/user/keymap/editor.lua
@@ -11,8 +11,10 @@ return {
 
 	["n|J"] = map_cmd("8jzz"):with_noremap():with_silent(),
 	["n|K"] = map_cmd("8kzz"):with_noremap():with_silent(),
-	["n|<F7>"] = map_cmd("8zh"):with_noremap():with_silent():with_desc("moving"),
-	["n|<F8>"] = map_cmd("8zl"):with_noremap():with_silent():with_desc("moving"),
+	-- ["n|<F7>"] = map_cmd("8zh"):with_noremap():with_silent():with_desc("moving"),
+	-- ["n|<F8>"] = map_cmd("8zl"):with_noremap():with_silent():with_desc("moving"),
+	["n|H"] = map_cmd("8zh"):with_noremap():with_silent():with_desc("moving"),
+	["n|L"] = map_cmd("8zl"):with_noremap():with_silent():with_desc("moving"),
 	["n|<Leader>gi"] = map_cr("GoImplements"):with_noremap():with_silent():with_desc("GoImplements"),
 	["n|<Leader>bo"] = map_cr("BufDelOthers"):with_noremap():with_silent():with_desc("BufDelOthers"),
 }
