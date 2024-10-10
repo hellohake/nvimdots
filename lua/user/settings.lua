048311d0affd1ccf337bc02f3185d435fff9045c
lihao.hellohake <lihao.hellohake@bytedance.com>
2024-10-10 16:07:42 +0800

    update

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -7,7 +7,6 @@ settings["colorscheme"] = "catppuccin"
 -- Filetypes in this list will skip lsp formatting if rhs is true.
 ---@type table<string, boolean>
 settings["formatter_block_list"] = {
-	--lua = false, -- example
 	thrift = false,
 }
 
